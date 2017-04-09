dfcd958e538adb87fbaed9bced0051b898fab34d
Luis Rocha <lerocha@gmail.com>
2017-04-08 17:35:52 -0700

    Filters - list of categories

---
+++ Yelp/Category.swift
@@ -0,0 +1,27 @@
+//
+//  Category.swift
+//  Yelp
+//
+//  Created by Rocha, Luis on 4/8/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class Category: NSObject {
+    let alias: String?
+    let title: String?
+    let parents: [String]?
+    
+    init(alias: String, title: String) {
+        self.alias = alias
+        self.title = title
+        self.parents = [String]()
+    }
+    
+    init(categoryDict: NSDictionary) {
+        self.alias = categoryDict["alias"] as? String
+        self.title = categoryDict["title"] as? String
+        self.parents = categoryDict["parents"] as? [String]
+    }
+}
--- Yelp/FiltersViewController.swift
+++ Yelp/FiltersViewController.swift
@@ -7,16 +7,22 @@
 //
 
 import UIKit
+import AFNetworking
 
-class FiltersViewController: UIViewController {
+class FiltersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var categories: [Category]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
 
         // Do any additional setup after loading the view.
+        categories = yelpCategories()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +37,33 @@ class FiltersViewController: UIViewController {
     @IBAction func onSearchButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchCell", for: indexPath) as! SwitchCell
+        cell.switchLabel.text = categories[indexPath.row].title
+        return cell
+    }
+    
+    func yelpCategories() -> [Category] {
+        var categories = [Category]()
+        
+        // read categories.json from Yelp
+        if let url = Bundle.main.url(forResource: "Data/categories", withExtension: "json") {
+            let data = try! Data(contentsOf: url)
+            if let json = try! JSONSerialization.jsonObject(with: data, options:[]) as? [NSDictionary] {
+                for dict in json {
+                    let category = Category(categoryDict: dict as NSDictionary)
+                    categories.append(category)
+                }
+            }
+        }
+        
+        return categories
+    }
     
     /*
     // MARK: - Navigation
@@ -41,5 +74,4 @@ class FiltersViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
